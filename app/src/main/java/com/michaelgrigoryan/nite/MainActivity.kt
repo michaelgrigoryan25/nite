a6ba8836e4e6ad7c17b879bcc861e1de1d3084cf
Michael <michael.grigoryan25@gmail.com>
2020-12-29 00:39:03 +0400

    UI Update

--- app/src/main/java/com/michaelgrigoryan/nite/MainActivity.kt
+++ app/src/main/java/com/michaelgrigoryan/nite/MainActivity.kt
@@ -28,7 +28,6 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
     }
 
     override fun onSupportNavigateUp(): Boolean {
-
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment
         val navController = navHostFragment.navController
 
